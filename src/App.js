84fe3cf05813f32228a532c0df6a9e695e8dab30
Taiwo Afolabi <luke.afolabi@outlook.com>
2021-02-15 00:39:46 -0800

    Filtered State SubChap20

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {  
+      persons: [],
+      searchField: '' 
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const persons = await response.json();
+    this.setState({ persons: persons}); 
+  }
+
+  render() {
+
+    const { persons, searchField } = this.state; 
+    const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return (
+      <div className="App">
+        <input type="search" placeholder="search persons" onChange={e => {
+          this.setState({ searchField: e.target.value }, () => console.log(this.state));
+        }} />
+        <CardList persons={filteredPersons} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
